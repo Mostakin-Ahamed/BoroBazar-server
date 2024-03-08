f6a1faa913c8d5ba5b3800c8b4af7f500d07ed3b
Mostakin-Ahamed <mostakinahamedwork@gmail.com>
2024-03-08 13:22:41 +0600

    single product found through id search

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mostakinahamed.fo1obhn.mongodb.net/?retryWrites=true&w=majority&appName=MostakinAhamed`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,12 +25,45 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("Boro-Bazar").collection("Items");
+
+    app.get('/items',async(req,res)=>{
+        const result = await database.find().toArray();
+        res.send(result)
+    })
+
+    app.get('/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await database.findOne(query);
+      res.send(result)
+    })
+
+
+
+
+    //post methods
+
+    app.post('/addItem', async(req, res)=>{
+      const newItem = req.body;
+      const result = await database.insertOne(newItem);
+      res.send(result)
+  })
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
