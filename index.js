5d8327baffd0c2c6990135f6e02c08752038212d
Mostakin-Ahamed <mostakinahamedwork@gmail.com>
2024-03-08 18:00:53 +0600

    done

--- index.js
+++ index.js
@@ -28,69 +28,93 @@ async function run() {
 
     const database = client.db("Boro-Bazar").collection("Items");
     const myCartDB = client.db("Boro-Bazar").collection("CartItems");
-    //get methods
-    app.get('/items', async (req, res) => {
-      const result = await database.find().toArray();
-      res.send(result)
-    })
-    //single item detail 
-    app.get('/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await database.findOne(query);
-      res.send(result)
-    })
-    //my cart info
-    app.get('/cart', async(req, res)=>{
-      const result = await myCartDB.find().toArray();
-      res.send(result)
-    })
 
-  //   app.get('/cartItems', async(req, res)=>{
-  //     const email = req.query.email;
-  //         let query ={};
-  //         if(req.query?.email){
-  //             query={email: email}
-  //         }
-  //     const result = await myCartDB.find(query).toArray();
-  //     res.send(result)
-  // })
-
-    
-
-
-    //post methods
-    //Sell Items
+    // Sell Items - Add Item
     app.post('/addItem', async (req, res) => {
-      const newItem = req.body;
-      const result = await database.insertOne(newItem);
-      res.send(result)
-    })
-    //Add to cart
+      try {
+        const newItem = req.body;
+        const result = await database.insertOne(newItem);
+        res.send(result);
+      } catch (error) {
+        console.error('Error adding item:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+
+    // get methods
+    app.get('/items', async (req, res) => {
+      try {
+        const result = await database.find().toArray();
+        res.send(result);
+      } catch (error) {
+        console.error('Error fetching items(/items ):', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+
+    // get method for specific item by id
+    app.get('/item/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await database.findOne(query);
+        if (!result) {
+          return res.status(404).send('Item not found');
+        }
+        res.send(result);
+      } catch (error) {
+        console.error('Error fetching item by id:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+
+    // Add to Cart
     app.post('/addToCart', async (req, res) => {
-      const cartItem = req.body;
-      const result = await myCartDB.insertOne(cartItem);
-      res.send(result)
+      try {
+        const cartItem = req.body;
+        const result = await myCartDB.insertOne(cartItem);
+        res.send(result);
+      } catch (error) {
+        console.error('Error adding item to cart:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+    // Get My Cart Info
+    app.get('/carts', async (req, res) => {
+      try {
+        const result = await myCartDB.find().toArray();
+        res.send(result);
+      } catch (error) {
+        console.error('Error fetching items from cart:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+    //  delete 
+    app.delete('/carts/delete/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await myCartDB.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        console.log("'error on app.delete('/carts/:id'", error)
+      }
     })
-
-
-
-
-
-
-
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
 
 
 app.get('/', (req, res) => {
