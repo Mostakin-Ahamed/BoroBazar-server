2e6e34008475b29ae4b33d0f163f2dc8a1c567cd
Mostakin-Ahamed <mostakinahamedwork@gmail.com>
2024-03-09 20:34:59 +0600

    vercel deoployed and done

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("Boro-Bazar").collection("Items");
     const myCartDB = client.db("Boro-Bazar").collection("CartItems");
